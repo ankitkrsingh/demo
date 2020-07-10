8f47a7aa14853cbdb36a1669acf79e2cf143ef42
Ankit Kumar Singh <ankitkumarsingh@MacBook-Pro.fritz.box>
2020-07-10 08:27:36 +0200

    First commit

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/")
+    public String getHelloWorld(){
+        return "<h1>Hello World!<h1>";
+    }
 }
